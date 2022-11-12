623bd5eb1f951d28b9614230209c1e92f494b96e
Havochalo <officialmail85@gmail.com>
2022-11-13 08:54:47 +1100

    update

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,5 +1,6 @@
 import React from "react";
 import prem from "../assets/prem.png";
+import Rectangle3image from "../assets/Rectangle3image.png";
 
 export const Hero = () => {
   return (
@@ -7,18 +8,18 @@ export const Hero = () => {
       <div class="container">
         <div class="row py-5">
           <div class="col-md-5 text-center order-md-2">
-            <img src={prem} alt="my image" width="90%" />
+            <img src={Rectangle3image} alt="my image" width="90%" />
           </div>
 
           <div class="col-md-7 mt-5 pt-5">
             hi 👋 i'm
-            <span class="fw-bold">Prem Acharya</span>
+            <span class="fw-bold">Roshan Lama</span>
             <div class="mt-2 mb-5 fs-3 fw-bold">
-              Software Engineer | Coding Mentor
+              Software Engineer | Coding Enthusiast
             </div>
             <p>
-              I love codiong and solving problems with code and teaching to
-              other what i know that makes me feel happy
+              I love coding and solving problems with code and learning what i
+              don't know that makes me feel happy
             </p>
             <button class="btn btn-danger">
               Download Resume <i class="fa-solid fa-download"></i>
